400b59c4d1a041c569ab52113c5ab52526ea6fa5
kavindadewmith <kavindadewmith@gmail.com>
2025-11-25 15:09:13 +0530

    feat: Remove Login and Signup pages as authentication is handled by Asgardeo

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import Index from "./pages/Index";
 import SearchResults from "./pages/SearchResults";
 import TripDetails from "./pages/TripDetails";
 import BookingPage from "./pages/BookingPage";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
 import ProfilePage from "./pages/ProfilePage";
 import NotFound from "./pages/NotFound";
 
@@ -25,8 +23,6 @@ const App = () => (
           <Route path="/search" element={<SearchResults />} />
           <Route path="/trip/:tripId" element={<TripDetails />} />
           <Route path="/booking/:tripId" element={<BookingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
           <Route path="/profile" element={<ProfilePage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
